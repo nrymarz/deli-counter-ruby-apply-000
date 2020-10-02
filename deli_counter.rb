97349c7d105e0fde51b6f475049f2d546e66272e
Nathan Rymarz <nrymarz@gmail.com>
2020-10-02 21:57:23 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -14,7 +14,7 @@ end
 
 def now_serving(line)
   if line.empty?
-    puts "There is nobody waiting to be served!."
+    puts "There is nobody waiting to be served!"
   else 
     puts "Currently serving #{line.shift}."
   end 
