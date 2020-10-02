d929f29a1829e04c0cf872d2bca6ea00493762c7
Nathan Rymarz <nrymarz@gmail.com>
2020-10-02 21:44:09 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,7 +4,8 @@ katz_deli = []
 def line(line)
   if line.empty?
     puts "The line is currently empty."
-  else
-    puts "The line is currently: #{
-    line.each do |person|"
+  end
 end
+
+def take_a_number(line,person)
+  line.push
