69487cbe028dcdc00bfd7e5d0d234527b673349c
Nathan Rymarz <nrymarz@gmail.com>
2020-10-02 21:32:13 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,15 @@
 # Write your code here.
+katz_deli = []
+
+def line(line)
+  if line.empty?
+    puts "The line is currently empty."
+  else
+    position = 1 
+    print "The line is currently: "
+    line.each do |person|
+      print "#{position}. #{person}. "
+      position += 1 
+    end
+  end
+end
